8a261939e17d68dec422451ab5793a40985d565c
SatyapriyaMahato <satyapriyamahto@gmail.com>
2023-08-28 15:13:23 +0530

    add smooth scroll

--- script.js
+++ script.js
@@ -7,19 +7,27 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
 
-const openModal = function () {
+
+/******************** Modal view ********************/
+
+
+/****** Open Modal function ******/
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
 
+/****** close Modal function ******/
 const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(btn => { btn.addEventListener('click', openModal); })
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -29,3 +37,10 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+
+/******************** scroll function ********************/
+
+document.addEventListener('click', function () {
+  section1.scrollIntoView({ behavior: 'smooth' });
+})
