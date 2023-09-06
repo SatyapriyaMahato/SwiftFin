1f90cb882451464a66c326b39a4c5864d53bf911
SatyapriyaMahato <satyapriyamahto@gmail.com>
2023-09-06 09:56:21 +0530

    add tabbed section functionality

--- script.js
+++ script.js
@@ -2,10 +2,16 @@
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
+
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
+
 const section1 = document.querySelector('#section--1');
+
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContent = document.querySelectorAll('.operations__content');
 
 
 /******************** Modal view ********************/
@@ -75,3 +81,25 @@ const imgObserver = new IntersectionObserver(loadImg, {
 });
 
 imgTargets.forEach(img => imgObserver.observe(img));
+
+/******************** Tabbed section ********************/
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+
+  // Guard clause
+  if (!clicked) return;
+
+  // Remove active classes
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+
+  // Activate tab
+  clicked.classList.add('operations__tab--active');
+  // Activate content area
+
+  document.querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+
+})
